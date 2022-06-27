6094890906bcc5ededbbb0444044ee7b55180746
Dragomir <poisonjawbd@gmail.com>
2022-06-27 22:28:46 +0300

    1)am folosit comanda if else ca sa pun anumite condtii atunci cand numarul este egal cu 0 2)am folosit de asemenea comanda .isdigit ca sa nu permitem crashuirea programei atunci cand tapam un text un numar ce este decimal sau cu minus se poate in cazul minusului ca alternativa doar sa folosesti sa pui conditita de  if numar_de_zile > 0:         return f"{numar_de_zile} de zile sunt {numar_de_zile * ore_pe_zi} {in_ce_masoara}"
        else:
            return "Ba am zis sa scri numar pozitiv"
            dar nu este necesar datorita comandei .isdigit

--- RESEBA.py
+++ RESEBA.py
@@ -1,10 +1,18 @@
 ore_pe_zi = 24
 in_ce_masoara = "ore"
-if
+
 def zile_la_ore(numar_de_zile):
-    return f"{numar_de_zile} de zile sunt {numar_de_zile * ore_pe_zi} {in_ce_masoara}"
+    if numar_de_zile > 0:
+        return f"{numar_de_zile} de zile sunt {numar_de_zile * ore_pe_zi} {in_ce_masoara}"
+
+    elif numar_de_zile == 0:
+        return f"Ig 0 works too in orice caz {numar_de_zile} de zile sunt {numar_de_zile * ore_pe_zi} {in_ce_masoara}"
+
 
 inputul_userului = input("pune numarul de zile\n")
-numarul_scris_userului = int(inputul_userului)
-rezultat = zile_la_ore(numarul_scris_userului)
-print(rezultat)
+if inputul_userului.isdigit():
+    numarul_scris_userului = int(inputul_userului)
+    rezultat = zile_la_ore(numarul_scris_userului)
+    print(rezultat)
+else:
+    print("ce ati tapat nu este un numar valid ")
