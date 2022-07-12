44db16c14b3d45b35abc300b36654baa78673782
Dragomir <poisonjawbd@gmail.com>
2022-07-12 23:33:43 +0300

    am folosit  dicionary pentru a stoca inputul de "zile" si "unitate" acestea vor inlocui variabilile ore = 24 se_masoara = ore si vor foliste astfel aceste informatii  stocate pe care leam accesat cu ajutorul cuvintelor cheievor face posibila calcularea a cate ore ori minute sunt in nr de zile inputate, am facut sa depinda calculul dupa cuvantul cheie de dupa  semnul de split (":") daca nu va fi folist cuvintele cheie va fi conversiune nesuportata daca am facut greseli gramatice then sorry im abit tired....

--- RESEBA.py
+++ RESEBA.py
@@ -1,15 +1,19 @@
-ore_pe_zi = 24
-in_ce_masoara = "ore"
-def zile_la_ore(numar_de_zile):
-        return f"{numar_de_zile} de zile sunt {numar_de_zile * ore_pe_zi} {in_ce_masoara}"
+def zile_la_ore(numar_de_zile, conversiune_unitate ):
+    if conversiune_unitate == "ore":
+        return f"{numar_de_zile} de zile sunt {24 * numar_de_zile} ore"
+    elif conversiune_unitate == "minute":
+        return f"{numar_de_zile} de zile sunt {24  * 60 * numar_de_zile} minute"
+    else:
+        return " nesuportata conversiune"
 inputul_userului = ""
+
 def calcule():
 
     try:
 
-        numarul_scris_userului = int(inputul_userului_element)
+        numarul_scris_userului = int(dictionary_lista_zile_unitati["zile"])
         if numarul_scris_userului > 0:
-            rezultat = zile_la_ore(numarul_scris_userului)
+            rezultat = zile_la_ore(numarul_scris_userului, dictionary_lista_zile_unitati["unitate"])
             print(rezultat)
         elif numarul_scris_userului == 0:
             print("e zero")
@@ -18,8 +22,10 @@ def calcule():
         print("Numarul invalid")
 
 while inputul_userului != "iesire":
-    inputul_userului = input("pune numarul de zile\n")
-    lista_de_zile = inputul_userului.split(",")
-    for inputul_userului_element in set(lista_de_zile):
-        calcule()
+    inputul_userului = input("pune numarul de zile si unitatea\n")
+    lista_de_zile_unitati = inputul_userului.split(":")
+    print(lista_de_zile_unitati)
+    dictionary_lista_zile_unitati = {"zile" : lista_de_zile_unitati[0], "unitate" : lista_de_zile_unitati[1]}
+    print(dictionary_lista_zile_unitati)
+    calcule()
 
